74ca02d8356f5a9e4f1b25c8a174440252bb8da7
mina <mina_gabriel84@hotmail.com>
2014-09-14 08:39:42 -0400

    Commit
    Volley

--- CopticStream-Beta/src/com/copticstream/copticstream/MainActivity.java
+++ CopticStream-Beta/src/com/copticstream/copticstream/MainActivity.java
@@ -1,8 +1,18 @@
 package com.copticstream.copticstream;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends Activity {
@@ -11,6 +21,34 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		JsonObjectRequest jsObjRequest = new JsonObjectRequest(
+				Request.Method.GET,
+				"http://copticstream.com/streams.asmx/HelloWorld", null,
+				new Response.Listener<JSONObject>() {
+
+					@Override
+					public void onResponse(JSONObject response) {
+						try {
+							Log.i("Response: ", response.getString("Message"));
+						} catch (JSONException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+
+				}, new Response.ErrorListener() {
+
+					@Override
+					public void onErrorResponse(VolleyError error) {
+						// TODO Auto-generated method stub
+
+					}
+				});
+
+		// Access the RequestQueue through your singleton class.
+		MySingleton.getInstance(this).addToRequestQueue(jsObjRequest);
+		
 	}
 	
 	public void playVideo(View view) {
